74fc0ff9cba1556b4463c8068b8bac73461d4cb6
MarceloLeite2604 <marceloleite2604@gmail.com>
2022-10-08 00:44:58 -0300

    Analyze bug when creating bean with dependencies

--- autoconfigure/src/main/java/com/figtreelake/corbeanprocessor/autoconfigure/ChainBeanDefinitionRegistryPostProcessor.java
+++ autoconfigure/src/main/java/com/figtreelake/corbeanprocessor/autoconfigure/ChainBeanDefinitionRegistryPostProcessor.java
@@ -4,11 +4,14 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 
 import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -18,9 +21,10 @@ import java.util.stream.Collectors;
  * A {@link BeanDefinitionRegistryPostProcessor} implementation to retrieve
  * {@link ChainLink} beans that implements the same type and concatenate them
  * to build a chain of responsibility.
+ *
  * @param <X> Defines a consistent {@link ChainLink} implementation that will
- *           be used throughout all methods on this class. It can be defined
- *           as a wildcard on the actual implementation.
+ *            be used throughout all methods on this class. It can be defined
+ *            as a wildcard on the actual implementation.
  */
 @RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 public class ChainBeanDefinitionRegistryPostProcessor<X extends ChainLink<X>> implements BeanDefinitionRegistryPostProcessor {
@@ -45,17 +49,51 @@ public class ChainBeanDefinitionRegistryPostProcessor<X extends ChainLink<X>> im
   @Override
   public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry beanDefinitionRegistry) throws BeansException {
     this.beanDefinitionRegistry = beanDefinitionRegistry;
+    Set<Class> arguments = new HashSet<>();
+    for (final String beanDefinitionName : beanDefinitionRegistry.getBeanDefinitionNames()) {
+//      System.out.println(beanDefinitionName);
+      final var beanDefinition = beanDefinitionRegistry.getBeanDefinition(beanDefinitionName);
+      if (beanDefinition.getBeanClassName() != null) {
+        try {
+//          System.out.println(beanDefinition.getBeanClassName());
+          final var clazz = Class.forName(beanDefinition.getBeanClassName());
+          if (ChainLink.class.isAssignableFrom(clazz)) {
+            final var parameterizedTypeContext1 = parameterizedTypesRetriever.retrieveForClass(clazz)
+                .stream()
+                .filter(parameterizedTypeContext ->
+                    ChainLink.class.equals(parameterizedTypeContext.getParameterizedType()
+                        .getRawType()))
+                .findFirst()
+                .orElseThrow();
+
+            final var argument = parameterizedTypeContext1.getArguments()
+                .values()
+                .iterator()
+                .next();
+
+            if (!arguments.contains(argument)) {
+              System.out.println(clazz);
+              beanDefinition.setPrimary(true);
+              arguments.add(argument);
+            }
+          }
+        } catch (ClassNotFoundException e) {
+          throw new RuntimeException(e);
+        }
+      }
+    }
   }
 
   @Override
   public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
-    this.configurableListableBeanFactory = configurableListableBeanFactory;
-
-    final var chainLinkBeanContexts = retrieveChainLinkBeanContexts();
-
-    final var firstChainLinksBeanContext = assembleChains(chainLinkBeanContexts);
-
-    firstChainLinksBeanContext.forEach(this::updateBeanDefinitionOnRegistryAndFactory);
+    // TODO Refactor logic to postProcessBeanDefinitionRegistry
+//    this.configurableListableBeanFactory = configurableListableBeanFactory;
+//
+//    final var chainLinkBeanContexts = retrieveChainLinkBeanContexts();
+//
+//    final var firstChainLinksBeanContext = assembleChains(chainLinkBeanContexts);
+//
+//    firstChainLinksBeanContext.forEach(this::updateBeanDefinitionOnRegistryAndFactory);
   }
 
   @SuppressWarnings("unchecked")
